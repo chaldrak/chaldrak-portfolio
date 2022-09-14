6704059c2ab0e54aa82772ce9ac517b290101742
Chaldrak <chaldrakus@gmail.com>
2022-09-14 12:10:21 +0100

    pages skills: programmming languages

---
+++ src/components/HeroSkills.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <p class="mx-auto mt-5 max-w-prose text-xl text-gray-500">
+            <slot></slot> 
+            Programming Languages
+        </p>
+        <div class="mt-12">
+          <div class="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
+            <div v-for="feature in features" :key="feature.name" class="pt-6">
+              <div class="flow-root rounded-lg bg-gray-100 px-6 pb-8">
+                <div class="-mt-6">
+                  <div>
+                    <span class="inline-flex items-center justify-center rounded-md bg-gray-50 border-2 border-indigo-600 p-3 shadow-lg">
+                        <img :src="feature.icon" class="h-8 w-8" :alt="feature.name" />
+                    </span>
+                  </div>
+                  <h3 class="mt-8 text-lg font-medium tracking-tight text-gray-900">{{ feature.name }}</h3>
+                  <p class="mt-5 text-base text-gray-500">{{ feature.description }}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+    features: {
+        type: Array,
+        default: [{name: "", description: "", icon: ""}]
+    }
+})
+</script>
+
+<style>
+
+</style>
--- src/pages/TheSkills.vue
+++ src/pages/TheSkills.vue
@@ -1,15 +1,46 @@
 <template>
-  <div class="min-h-screen">
-    Skills
-  </div>
+    <div class="relative bg-white py-10 sm:py-16 lg:py-24">
+      <div class="mx-auto max-w-md px-4 text-center sm:max-w-3xl sm:px-6 lg:max-w-7xl lg:px-8">
+        <h2 class="text-lg font-semibold text-indigo-600">My Skills</h2>
+        <HeroSkills :features="languages">💫</HeroSkills>
+      </div>
+    </div>
 </template>
-
-<script>
-export default {
-
-}
+  
+<script setup>
+import HeroSkills from '../components/HeroSkills.vue';
+  
+const languages = [
+    {
+      name: 'C',
+      description: 'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.',
+      icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/c/c-original.svg"
+,
+    },
+    {
+      name: 'Java',
+      description: 'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.',
+      icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+    },
+    {
+      name: 'JavaScript',
+      description: 'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.',
+      icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg",
+    },
+    {
+      name: 'TypeScript',
+      description: 'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.',
+      icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg",
+    },
+    {
+      name: 'PHP',
+      description: 'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.',
+      icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/php/php-original.svg",
+    },
+    {
+      name: 'Ruby',
+      description: 'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.',
+      icon: "	https://raw.githubusercontent.com/devicons/devicon/master/icons/ruby/ruby-original.svg",
+    }
+  ]
 </script>
-
-<style>
-
-</style>
